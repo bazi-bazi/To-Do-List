bda6c3155cc9fdbe14773e328e60881b10370832
bazi <vazha.davituliani@gmail.com>
2020-12-01 17:20:55 +0400

    Updates

---
+++ precache-manifest.495034cefd0a559d40b0e2eb42b4ef23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b5441d00ca720f55e7c25de62b652a",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "5ba9b68139f5e485a084",
+    "url": "/To-Do-List/static/css/main.562488f9.chunk.css"
+  },
+  {
+    "revision": "01d44dd61367252c5c4c",
+    "url": "/To-Do-List/static/js/2.a5da75a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/To-Do-List/static/js/2.a5da75a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ba9b68139f5e485a084",
+    "url": "/To-Do-List/static/js/main.3df3f18c.chunk.js"
+  },
+  {
+    "revision": "e71b0fca7b414be357cd",
+    "url": "/To-Do-List/static/js/runtime-main.8e2d492d.js"
+  }
+]);
